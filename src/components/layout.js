be84b9494c13139f248f29f7104582f43b362f8b
Chad Carbert <chadcarbert@me.com>
2020-01-14 20:19:59 -0500

    Remove footer and link header to root

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,25 +11,18 @@ class Layout extends React.Component {
       <>
         <div class="page-container">
           <header class="header">
-            <h1 class="header__site-title">sticksnglue</h1>
-            <p class="header__tag">building for the fun of it</p>
+            <Link to="/">
+              <h1 class="header__site-title">sticksnglue</h1>
+              <p class="header__tag">building for the fun of it</p>
+            </Link>
             <div class="header__angle-bracket"></div>
             <div class="header__curly-bracket"></div>
           </header>
         </div>
 
-
         <div class="page-container">
-        <main class="main-content">
-            <main>{children}</main>
-        </main>
-      </div>
-
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+          <main class="main-content">{children}</main>
+        </div>
       </>
     )
   }
